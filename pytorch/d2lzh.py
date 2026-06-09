f5651e8ed417913e110339a9b23e4e45b03bbf37
yuan-sketch <yuanchao0401@qq.com>
2026-06-09 15:04:19 +0800

    更新

--- pytorch/d2lzh.py
+++ pytorch/d2lzh.py
@@ -18,8 +18,8 @@ from torch import nn
 import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
-import torchtext
-import torchtext.vocab as Vocab
+#import torchtext
+#import torchtext.vocab as Vocab
 import numpy as np
 
 VOC_CLASSES = ['background', 'aeroplane', 'bicycle', 'bird', 'boat',
